137a0c7da0b3a7cecc9a6f483c6c694a5f88ff35
psatomas <psatomas@gmail.com>
2025-03-31 19:06:32 -0300

    feat: updated main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 fn main() {
-    let pi: f64 = 3.1415926535897932384;
-    println!("The current value or pi is {pi}");
+    let miles_away = 50;
+    let miles_away_i8 = miles_away as i8;
+    let miles_away_u8 = miles_away as u8;
 
-    println!("{}", pi.floor());
-    println!("{}", pi.ceil());
-    println!("{}", pi.round());
+    let miles_away = 100.329032;
+    let miles_away_f32 = miles_away as f32;
+    let miles_away_int = miles_away as i32;
+    println!("{miles_away_int}");
 }
