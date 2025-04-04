3a2de2dbca800d58d8f90a9d054ccbb63d1d5152
psatomas <psatomas@gmail.com>
2025-04-04 15:05:09 -0300

    feat: updated main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 fn main() {
-    let is_hansome = true;
-    let is_silly: bool = false;
+    println!("{}", !true);
+    println!("{}", !false);
 
-    println!("handsome: {is_handsome}. Silly: {is_silly");
-
-    let age: i32 = -40;
-    let is_young = age < 35;
-    println!("{is_young}");
-    println!("{} {}", age.is_positive(), age.is_negative())
+    let age = 13;
+    let can_see_rated_r_movie = age >= 17;
+    let cannot_see_rated_r_movie = !can_see_rated_r_movie;
+    println!("I am {age} years old. Can I see this scary movie?{can_see_rated_r_movie}");
 }
