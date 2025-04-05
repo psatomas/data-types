21510feba7ec1df32a29ca43ba1d9c3a302acf29
psatomas <psatomas@gmail.com>
2025-04-05 14:37:39 -0300

    feat: updated main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
 fn main() {
-    println!("{}", !true);
-    println!("{}", !false);
-
-    let age = 13;
-    let can_see_rated_r_movie = age >= 17;
-    let cannot_see_rated_r_movie = !can_see_rated_r_movie;
-    println!("I am {age} years old. Can I see this scary movie?{can_see_rated_r_movie}");
+    let purchased_ticket = true;
+    let plane_on_time = true;
+    let making_event = purchased_ticket && plane_on_time;
+    println!("It is {} that I will arrive as expected", making_event);
 }
