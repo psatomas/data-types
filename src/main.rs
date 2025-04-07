2ecd9f9c2e4d36cc982e74fd0bce0f94a9214506
psatomas <psatomas@gmail.com>
2025-04-07 15:34:42 -0300

    feat: updated main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 fn main() {
-    let user_has_paid_for_subscription = true;
-    let user_is_admin = true;
-    let user_can_see_premium_experience = user_has_paid_for_subscription || user_is_admin;
-    println!("Can this user see my site?{user_can_see_premium_experience}");
+    let seasons = ["Spring", "Summer", "Fall", "Winter"];
+
+    println!("{}", 5);
+    println!("{}", 3.14);
+    println!("{}", true);
+    print!("{:#?}", seasons);
 }
