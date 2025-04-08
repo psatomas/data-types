e70926942562f32d6544f108108485ea451b5bea
psatomas <psatomas@gmail.com>
2025-04-08 15:21:38 -0300

    feat: updated main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 fn main() {
-    let seasons = ["Spring", "Summer", "Fall", "Winter"];
+    let employee = ("Molly", 32, "Marketing");
 
-    println!("{}", 5);
-    println!("{}", 3.14);
-    println!("{}", true);
-    print!("{:#?}", seasons);
+    //let name = employee.0;
+    //let age = employee.1;
+    //let department = employee.2;
+
+    let (name, age, department) = employee;
+
+    println!("Name: {name}, age: {age}, department: {department}");
+
+    println!("{employee:#?}")
 }
